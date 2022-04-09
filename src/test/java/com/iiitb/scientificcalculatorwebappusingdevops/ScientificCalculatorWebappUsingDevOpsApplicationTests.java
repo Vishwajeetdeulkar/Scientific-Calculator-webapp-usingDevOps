dbec145de79b67008666101d78053e850a3153f8
vishwa <vishwajeetd13@gmail.com>
2022-04-09 15:12:32 +0530

    Initial commit : Bare project VCS setup

---
+++ src/test/java/com/iiitb/scientificcalculatorwebappusingdevops/ScientificCalculatorWebappUsingDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitb.scientificcalculatorwebappusingdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificCalculatorWebappUsingDevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
